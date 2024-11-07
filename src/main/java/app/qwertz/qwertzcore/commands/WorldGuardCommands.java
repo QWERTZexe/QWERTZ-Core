16a98e33f44ad2de4a9deab3024b5f6940613bf5
NotAzex <azexdid@gmail.com>
2024-11-07 21:23:59 +0100

    Added Flowing
    Added /toggleflow & /flow
    Added a method that auto-enables high frequency flags in WorldGuard config when /flow is ran
    Unsolved issue: when config is reloaded you are unable to toggle flowing.
    
    (cherry picked from commit 4f95eb2992082277137bcb60905e5008e54ed092)

--- src/main/java/app/qwertz/qwertzcore/commands/WorldGuardCommands.java
+++ src/main/java/app/qwertz/qwertzcore/commands/WorldGuardCommands.java
@@ -4,22 +4,42 @@ import app.qwertz.qwertzcore.QWERTZcore;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WorldGuardCommands implements CommandExecutor {
 
     private final QWERTZcore plugin;
     public WorldGuardCommands(QWERTZcore plugin) {
         this.plugin = plugin;
+    }
+
+    public boolean enableHighFrequencyFlags() {
+        File configFile = new File(WorldGuard.getInstance().getPlatform().getGlobalStateManager().getDataFolder(), "config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+        config.set("regions.high-frequency-flags", true);
+        try {
+            config.save(configFile);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "wg reload");
+            return true;
+        } catch (IOException e) {
+            this.plugin.getLogger().warning("We were unable to enable 'high-frequency-flags' option in the WorldGuard config. /flow will be broken. ");
+            return false;
+        }
     }
 
     @Override
@@ -69,6 +89,11 @@ public class WorldGuardCommands implements CommandExecutor {
         String flagName;
 
         switch (command.getName().toLowerCase()) {
+            case "flow":
+            case "toggleflow":
+                flag = Flags.WATER_FLOW;
+                flagName = "FLOWING";
+                break;
             case "pvp":
             case "togglepvp":
                 flag = Flags.PVP;
@@ -99,8 +124,15 @@ public class WorldGuardCommands implements CommandExecutor {
         }
 
         boolean currentState = region.getFlag(flag) != StateFlag.State.DENY;
-        region.setFlag(flag, currentState ? StateFlag.State.DENY : StateFlag.State.ALLOW);
-
+        if (flagName != "FLOWING") {
+            region.setFlag(flag, currentState ? StateFlag.State.DENY : StateFlag.State.ALLOW);
+        }else{
+            StateFlag[] FlowFlags = {Flags.WATER_FLOW, Flags.LAVA_FLOW};
+            enableHighFrequencyFlags();
+            for (StateFlag loop : FlowFlags) {
+                region.setFlag(loop, currentState ? StateFlag.State.DENY : StateFlag.State.ALLOW);
+            }
+        }
         String scope = isGlobal ? plugin.getConfigManager().getColor("colorSecondary") + " GLOBALLY" : " in region " + plugin.getConfigManager().getColor("colorSecondary") + "'" + region.getId() + "'";
         String newState = currentState ? plugin.getConfigManager().getColor("colorDead") + "DISABLED" : plugin.getConfigManager().getColor("colorAlive") + "ENABLED";
         plugin.getMessageManager().broadcastMessage(QWERTZcore.CORE_ICON + " " + plugin.getConfigManager().getColor("colorPrimary") + sender.getName() + plugin.getConfigManager().getColor("colorSuccess") + " just " + newState + " " + plugin.getConfigManager().getColor("colorPrimary") + flagName + plugin.getConfigManager().getColor("colorSuccess") + scope + plugin.getConfigManager().getColor("colorSuccess") + "!");
